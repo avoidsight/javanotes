66d9ec860bb19c88bb41e30440c607d6da1b4821
avoidsight <313694179@qq.com>
2019-06-02 14:33:48 +0800

    一个简易的tomcat

---
+++ tomcat/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+public class HelloWorldServlet extends MyServlet{
+
+    @Override
+    public void doGet(MyRequest myRequest,MyRespond myRespond){
+        try {
+            myRespond.write("get world...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void doPost(MyRequest myRequest,MyRespond myRespond){
+        try {
+            myRespond.write("post world...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
