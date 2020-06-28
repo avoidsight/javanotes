480e1eb293628ab0366dc7573afb8137f15d0701
avoidsight <menmenz1995@gmailcom>
2020-06-28 13:49:17 +0800

    leetcode 算法139，209

---
+++ com.zp/leetcode/Solution209.java
@@ -0,0 +1,28 @@
+package com.zp.leetcode;
+
+/**
+ * @author menmenz
+ * @version 1.0
+ * @date 2020-06-28 12:38
+ */
+public class Solution209 {
+    public int minSubArrayLen(int s, int[] nums) {
+        int res = 0;
+        int sum =0;
+        int j = 0;
+        for(int i =0; i<nums.length; i++){
+            sum = sum+nums[i];
+            while(sum>=s){
+                res = res==0? i-j+1:Math.min(res,i-j+1);
+                sum -= nums[j++];
+
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Solution209 solution209 = new Solution209();
+        System.out.println(solution209.minSubArrayLen(6, new int[]{2, 3, 1, 2, 4, 3}));
+    }
+}
