fc5f9fda209a68411722d1584cf5d492f1c063d9
avoidsight <menmenz1995@gmailcom>
2020-07-07 09:22:24 +0800

    leetcode 算法 112

---
+++ com.zp/leetcode/TreeNode.java
@@ -0,0 +1,13 @@
+package com.zp.leetcode;
+
+/**
+ * @author menmenz
+ * @version 1.0
+ * @date 2020-07-07 09:10
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
