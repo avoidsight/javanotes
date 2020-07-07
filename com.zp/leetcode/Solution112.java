3de1de85eba40927e1b62c71647ba49fe792006c
avoidsight <menmenz1995@gmailcom>
2020-07-07 09:29:55 +0800

    leetcode 算法 112 简化

--- com.zp/leetcode/Solution112.java
+++ com.zp/leetcode/Solution112.java
@@ -8,15 +8,8 @@ package com.zp.leetcode;
 public class Solution112 {
     public boolean hasPathSum(TreeNode root, int sum) {
         if(root==null) return false;
-        if(root.left == null && root.right == null){
-            if(sum == root.val) {
-                return true;
-            }else{
-                return false;
-            }
-        }else{
-            return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum - root.val);
-        }
+        if(root.left == null && root.right == null) return sum==root.val;
+        return hasPathSum(root.left,sum - root.val) || hasPathSum(root.right,sum - root.val);
     }
 
     public static void main(String[] args) {
