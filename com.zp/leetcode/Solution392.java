d8335fc02efafd34bc3507566555aadbfbc59f85
avoidsight <menmenz1995@gmailcom>
2020-07-27 14:11:34 +0800

    leetcode 392

---
+++ com.zp/leetcode/Solution392.java
@@ -0,0 +1,26 @@
+package com.zp.leetcode;
+
+/**
+ * @author menmenz
+ * @version 1.0
+ * @date 2020-07-27 13:29
+ */
+public class Solution392 {
+    public boolean isSubsequence(String s, String t) {
+        int m = 0,max = s.length();
+        for(int i = 0;i<t.length();i++){
+            if(s.charAt(m) == t.charAt(i)){
+                if(m==max-1){
+                    return true;
+                }
+                m++;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution392 solution392 = new Solution392();
+        System.out.println(solution392.isSubsequence("axc","ahbgdc"));
+    }
+}
